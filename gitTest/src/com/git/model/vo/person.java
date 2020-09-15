a97a571ae43a0faae7c2cf27d0eda366e53637d5
skygu <skygu@DESKTOP-3R7QBTV>
2020-09-15 11:08:50 +0900

    01_person add properties name age -han-

--- gitTest/src/com/git/model/vo/person.java
+++ gitTest/src/com/git/model/vo/person.java
@@ -2,5 +2,32 @@ package com.git.model.vo;
 
 public class person {
 	//회원정보 저장하는 클래스 
+	private String name;
+	private int age;
+	
+	public person() {}
+
+	public person(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
 	
 }
