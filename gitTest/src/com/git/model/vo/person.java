b8ace4a9f00d599f93b56ac4694b6caa290bae21
master <skygu@DESKTOP-3R7QBTV>
2020-09-15 11:16:39 +0900

    04_person add 주소 키 -han-

--- gitTest/src/com/git/model/vo/person.java
+++ gitTest/src/com/git/model/vo/person.java
@@ -4,14 +4,16 @@ public class person {
 	//회원정보 저장하는 클래스 
 	private String name;
 	private int age;
+	private String address;
+	private double height; 
 	
 	public person() {}
 
-	public person(String name, int age) {
-		super();
-		this.name = name;
-		this.age = age;
-	}
+//	public person(String name, int age, String address, double height) {
+//		super();
+//		this.name = name;
+//		this.age = age;
+//	}
 
 	public String getName() {
 		return name;
@@ -28,6 +30,24 @@ public class person {
 	public void setAge(int age) {
 		this.age = age;
 	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	
 	
 	
 }
